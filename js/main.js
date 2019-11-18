945dd1cd4e39dc174ffd7b4269c9b70f2b2ec722
Mihai <mihainuica@gmail.com>
2019-11-18 20:08:31 +0000

    removed separate html files for different search strategies. instead there will be a drop-down on the main page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+
+
+let node = new Node(1, 2);
+
+console.log(node);
+console.log(node.parentNode);
---
+++ js/node.js
@@ -0,0 +1,11 @@
+class Node {
+    constructor(state,parentNode,action,pathCost,depth){
+        this.state = state;//string representing n*n size grid
+        this.parentNode = parentNode;//parent node in the tree. null if root node
+        this.action = action;//action taken to get to the current state
+        this.pathCost = pathCost;//to be implemented in A*
+        this.depth = depth;//depth level of the node aka # of parent nodes
+    }
+    
+
+}
